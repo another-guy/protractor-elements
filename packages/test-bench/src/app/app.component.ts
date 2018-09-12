91af1ea76e60207aca45c4978f4edf527621bbff
Igor Soloydenko <soloydenko@gmail.com>
2018-09-11 21:31:06 -0700

    Use protractor-element's greeter from test-bench

--- packages/test-bench/src/app/app.component.ts
+++ packages/test-bench/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+import { Greeter } from 'protractor-elements';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'test-bench';
+  title = 'test-bench ' + new Greeter('x').greet();
 }
