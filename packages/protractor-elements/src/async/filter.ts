7520ce15747b4e60da648186293aecceecc3eea3
Igor Soloydenko <soloydenko@gmail.com>
2018-09-19 09:51:22 -0700

    Remove unused 'async' folder

--- packages/protractor-elements/src/async/filter.ts
+++
@@ -1,8 +0,0 @@
-export async function filter$<T>(
-  target: T[],
-  filterFn: (_: T) => Promise<boolean>
-): Promise<T[]> {
-  const result: T[] = [];
-  for (const item of target) if (await filterFn(item)) result.push(item);
-  return result;
-}
